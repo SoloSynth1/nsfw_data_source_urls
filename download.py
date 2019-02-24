d9fdb522b531ae947406de794dc11d726bcb71e8
OrixAY <orix.auyeung@hunt.social>
2019-02-24 13:36:38 +0800

    refactor

--- download.py
+++ download.py
@@ -1,6 +1,5 @@
 import threading
-from os import path
-from os import listdir
+from os import path, listdir
 import requests
 
 data_path = path.abspath("./raw_data")
@@ -21,7 +20,7 @@ def download_urls(file_path, thread_count=50):
         urls = f.read().split('\n')
     valid_urls = [(url, get_file_name(url, file_path)) for url in urls if not path.isfile(get_file_name(url, file_path))]
     url_count = len(valid_urls)
-    threads = [threading.Thread(target=download_manager, args=(valid_urls,)) for _ in range(min(thread_count,url_count))]
+    threads = [threading.Thread(target=download_manager, args=(valid_urls,)) for _ in range(min(thread_count, url_count))]
     for thread in threads:
         thread.start()
 
@@ -30,6 +29,7 @@ def download_manager(urls_pool):
     while urls_pool:
         url, file_name = urls_pool.pop(0)
         download(url, file_name)
+
 
 def download(url, file_name, timeout=5, retries_max=3):
     print("{}  ==>  {}...".format(url, file_name))
@@ -40,11 +40,10 @@ def download(url, file_name, timeout=5, retries_max=3):
             if response.status_code < 400:
                 write(response.content, file_name)
                 break
-            else:
-                retries += 1
-        except Exception:
-            retries += 1
+        except:
             continue
+        finally:
+            retries += 1
 
 
 def get_file_name(url, file_path):
